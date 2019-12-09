91a182a804149d3642702c8c082d4033fa5aa79a
Rayan Khan <rayankhan04@gmail.com>
2019-12-09 13:23:41 -0600

    Added (Basic) start screen logic. Final start screen will have custom buttons and moving background.

---
+++ Mobile App Programming Game/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Mobile App Programming Game
+//
+//  Created by Khan, Rayan A on 12/5/19.
+//  Copyright © 2019 Khan, Rayan A. All rights reserved.
+//
+
+class ViewController: UIViewController {
+    
+ 
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
